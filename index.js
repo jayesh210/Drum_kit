5974c8306a7aadd95107335ab54c42ce15e76fee
Jayesh Paliwal <56557919+jayesh210@users.noreply.github.com>
2020-07-09 14:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function buttonAnimation(currentKey){
     
     activeButton.classList.add("pressed");
    
-    setTimeout(function(){ activeButton.classList.remove("pressed");},100
+    setTimeout(function(){ activeButton.classList.remove("pressed");},30
     )
     
     
